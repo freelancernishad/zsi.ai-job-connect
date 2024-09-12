4ac325f18d3554570f9496b14ae0e17a488dd21e
freelancernishad <freelancernishad123@gmail.com>
2024-09-12 12:41:50 +0600

    update

--- app/Http/Controllers/Global/GlobalUserController.php
+++ app/Http/Controllers/Global/GlobalUserController.php
@@ -29,6 +29,7 @@ class GlobalUserController extends Controller
             'employmentHistory',
             'preferredJobTitleService'
         ]);
+        return $users = $query->paginate(10);
 
     // Handle preferred_job_title filter
     if ($request->has('preferred_job_title')) {
